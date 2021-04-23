1d4b0ea02b7cc0451a264a6960f9d59254195e29
Anay2707 <75680799+Anay2707@users.noreply.github.com>
2021-04-23 18:41:58 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -10,16 +10,15 @@ class Bob{
         this.x = x;
         this.y = y;
         this.r = r;
-        this.image = loadImage("paper.png");
+        this.body = Bodies.circle(x,y,options);
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.r, this.r);
+        eclipseMode(CENTER);
+        circle(this.body, 0, 0, this.r);
         pop();
         
       }
--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var bobDiameter
+var bobDiameter = 30;
 
 function preload()
 {
